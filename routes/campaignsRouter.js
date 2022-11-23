30c3b33bdc77c61dc7d713caef58a3cbe3824e5c
birdsnbears <louischu15@gmail.com>
2022-11-22 19:54:05 -0800

    Added a GET, PATCH, DELETE, and POST for /campaigns
    for the GET endpoints, there is one with and without an id. without an ID it will return all campaigns in the db. with an id it will return that specific campaign with that given id.

--- routes/campaignsRouter.js
+++ routes/campaignsRouter.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Campaign = require('../models/campaign');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-	res.send('DEATH TO ALL.');
-});
-
-router.get('/test', async (req, res) => {
+/* GET all campaigns in the database */
+// currently it returns all the campaigns in the campaign db.
+router.get('/', async (req, res) => {
 	try {
 		const campaigns = await Campaign.find();
 		res.json(campaigns);
@@ -15,8 +12,40 @@ router.get('/test', async (req, res) => {
 		console.error(error.message);
 		res.status(500).json({ message: error.message });
 	}
+});
+
+/* GET a specific campaign listing by id. */
+router.get('/:id', getCampaign, function (req, res) {
+	res.json(res.campaign);
+});
+
+/* PATCH a specific campaign listing by id. */
+router.patch('/:id', getCampaign, async (req, res) => {
+	// update the given campaign information
+	let updatedKeys = Object.keys(req.body);
+	updatedKeys.forEach(key => {
+		res.campaign[key] = req.body[key];
+	});
+
+	try {
+		const updatedCampaign = await res.campaign.save();
+		res.json(updatedCampaign);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 })
 
+/* DELETE a specific campaign listing by id. */
+router.delete('/:id', getCampaign, async (req, res) => {
+	try {
+		await res.campaign.remove();
+		res.json({ message: 'Deleted Campaign' });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+})
+
+/* POST a new campaign! */
 router.post('/', async (req, res) => {
 	const campaign = new Campaign({
 		title: req.body.title,
@@ -32,6 +61,27 @@ router.post('/', async (req, res) => {
 	} catch (error) {
 		res.status(400).json(error);
 	}
-})
+});
+
+/**
+ * This is a middleware function that is to be used whenever we expect a campaign ID from the client
+ * We search the database for a campaign with the given id.
+ * If something random goes wrong, return status 500
+ * If we cannot find a matching campaign, return status 404
+ */
+async function getCampaign(req, res, next) {
+	let campaign;
+	try {
+		campaign = await Campaign.findById(req.params.id);
+		if (campaign == null) {
+			return res.status(404).json({ message: 'Cannot find campaign' });
+		}
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+
+	res.campaign = campaign;
+	next();
+};
 
 module.exports = router;
