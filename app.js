d4668eedf6cb9b7110336bdb10ed0dc24f33ffbe
birdsnbears <louischu15@gmail.com>
2022-11-30 21:11:20 -0800

    removed indexrouter and added imagesrouter

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/usersRouter');
 var campaignsRouter = require('./routes/campaignsRouter');
+var imagesRouter = require('./routes/imagesRouter');
 
 var app = express();
 
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/campaigns', campaignsRouter);
+app.use('/images', imagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
