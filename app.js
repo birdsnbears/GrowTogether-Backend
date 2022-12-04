3efde139653ff17748b8be5169f7619320571761
birdsnbears <louischu15@gmail.com>
2022-12-03 17:05:46 -0800

    Implemented Image Upload

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var usersRouter = require('./routes/usersRouter');
-var campaignsRouter = require('./routes/campaignsRouter');
-var imagesRouter = require('./routes/imagesRouter');
+var usersRouter = require("./routes/usersRouter");
+var campaignsRouter = require("./routes/campaignsRouter");
+var imagesRouter = require("./routes/imagesRouter");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use('/users', usersRouter);
-app.use('/campaigns', campaignsRouter);
-app.use('/images', imagesRouter);
+app.use("/users", usersRouter);
+app.use("/campaigns", campaignsRouter);
+app.use("/images", imagesRouter);
 
 module.exports = app;
--- routes/imagesRouter.js
+++ routes/imagesRouter.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
 var router = express.Router();
 const Image = require("../models/image");
 
@@ -8,6 +9,42 @@ router.get("/", async function (req, res) {
   await createDefaultIfEmpty();
   const result = await Image.find();
   res.json(result);
+});
+
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "temp.png");
+  },
+});
+
+var upload = multer({ storage: storage });
+
+/* POST new Image */
+router.post("/", upload.single("image"), (req, res) => {
+  var obj = {
+    content: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/../public/images/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  Image.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send();
+    } else {
+      // item.save();
+      // res.redirect("/");
+      // res.status(201);
+      return res.status(201).send(item._id);
+    }
+  });
 });
 
 /* GET Image */
